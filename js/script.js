902c1f962316523a24a8db05dec283da8bdf8b81
Kristyn Ginski <missginski@gmail.com>
2018-12-10 17:59:58 -0500

    smooth scroll nav

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// portfolio buttons
 $('.my-btn').click(function() {
   let value = $(this).attr('data-filter');
 
@@ -23,6 +24,7 @@ $('.my-btn').click(function() {
 //   else ('.navbar').removeClass('bg-dark');
 // })
 
+	// for sticky navigation
 $('.features').waypoint(function(direction) {
   if (direction === 'down') {
     $('nav').addClass('sticky');
@@ -33,4 +35,45 @@ $('.features').waypoint(function(direction) {
     $('nav').removeClass('navbar-light');
     $('nav').addClass('navbar-dark');
   }
-})
+}
+, {
+  offset:' 140px;'
+}
+);
+
+// navigation scroll
+	$('a[href*="#"]')
+	  // Remove links that don't actually link to anything
+	  .not('[href="#"]')
+	  .not('[href="#0"]')
+	  .click(function(event) {
+	    // On-page links
+	    if (
+	      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+	      &&
+	      location.hostname == this.hostname
+	    ) {
+	      // Figure out element to scroll to
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+	      // Does a scroll target exist?
+	      if (target.length) {
+	        // Only prevent default if animation is actually gonna happen
+	        event.preventDefault();
+	        $('html, body').animate({
+	          scrollTop: target.offset().top
+	        }, 1000, function() {
+	          // Callback after animation
+	          // Must change focus!
+	          var $target = $(target);
+	          $target.focus();
+	          if ($target.is(":focus")) { // Checking if the target was focused
+	            return false;
+	          } else {
+	            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+	            $target.focus(); // Set focus again
+	          };
+	        });
+	      }
+	    }
+	  });
